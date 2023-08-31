19b0b10dd0b745e7793ce199f6de6d4b717c1a8d
“bruno <bruno_andradebr@hotmail.com>
2023-08-31 14:32:54 -0300

    Requisito - Adicionar intens ao carrinho - ok

--- cypress/e2e/adiciona-itens-carrinho.spec.cy.js
+++ cypress/e2e/adiciona-itens-carrinho.spec.cy.js
@@ -5,10 +5,9 @@ describe("Teste de Adição de Produto ao Carrinho", () => {
         cy.visit('https://automationexercise.com/products');
     });
 
-    it("Deve adicionar um produto ao carrinho de compras", () => {
+    it.only("Deve adicionar um produto ao carrinho de compras", () => {
         // Localize o botão "Add to Cart" do terceiro produto na lista e clique nele
         cy.get(':nth-child(3) > .product-image-wrapper > .single-products > .productinfo > .btn').click();
-
 
     });
 });
