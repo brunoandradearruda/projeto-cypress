baf606bd3a363864db1d70c717a0643cc24541c8
“bruno <bruno_andradebr@hotmail.com>
2023-09-01 14:30:37 -0300

    Requisito - Finalização de Compra - ok

--- cypress/e2e/adiciona-itens-carrinho.spec.cy.js
+++ cypress/e2e/adiciona-itens-carrinho.spec.cy.js
@@ -5,7 +5,7 @@ describe("Teste de Adição de Produto ao Carrinho", () => {
     });
 
 
-    it.only("Deve realizar uma pesquisa por um produto existente", () => {
+    it("Deve realizar uma pesquisa por um produto existente e específico", () => {
         const productToSearch = 'dress';
 
         cy.get('#search_product').type(productToSearch);
@@ -15,9 +15,8 @@ describe("Teste de Adição de Produto ao Carrinho", () => {
     });
 
 
-    it("Deve adicionar um produto ao carrinho de compras", () => {
-        // Localize o botão "Add to Cart" do terceiro produto na lista e clique nele
-        cy.get(':nth-child(3) > .product-image-wrapper > .single-products > .productinfo > .btn').click()
+    it.only("Deve adicionar um produto da pagína inicial de produtos ao carrinho de compras", () => {
+        cy.get(':nth-child(4) > .product-image-wrapper > .single-products > .productinfo > .btn').click()
 
     });
 });
--- cypress/e2e/finalizar-compra.spec.cy.js
+++ cypress/e2e/finalizar-compra.spec.cy.js
@@ -1,40 +1,37 @@
 import ticketsPage from "../Pages/Tickets/TicketsPage";
 
-describe("TC01 - Teste de Login e Finalização de Compra", () => {
-  it.only("Deve realizar login e finalizar a compra com sucesso", () => {
+describe("Teste de Login e Finalização de Compra", () => {
+  it("TC01 - Deve realizar login e finalizar a compra com sucesso", () => {
     // Realizar login
     cy.visit("https://automationexercise.com/login");
-    cy.get('[data-qa="login-email"]').type("brunoarruda7777@gmail.com.br");
-    cy.get('[data-qa="login-password"]').type("123456");
+
+
+    const login = 'brunoarruda7777@gmail.com.br'
+    const password = '123456'
+    cy.get('[data-qa="login-email"]').type(login);
+    cy.get('[data-qa="login-password"]').type(password);
+
     cy.get('[data-qa="login-button"]').click();
-  });
-
-  it("TC02 - Adiciona itens ao carrinho", () => {
-    // Localize o botão "Add to Cart" do terceiro produto na lista e clique nele
-    cy.get('.features_items > :nth-child(3) > .product-image-wrapper > .single-products > .productinfo > .btn').click();
-  });
-
-  it("TC03 - Exibir página do carrinho", () => {
-    // Localize o botão "Add to Cart" do terceiro produto na lista e clique nele
-    cy.get(".shop-menu > .nav > :nth-child(3) > a").click();
-  });
-
-  it("TC04 - Página do Checkout", () => {
-    // Localize o botão "Add to Cart" do terceiro produto na lista e clique nele
-    cy.get(".shop-menu > .nav > :nth-child(3) > a").click(); //Botão Proceed to Checkout
-  });
-
-  it("TC05 - Página de confirmação do endereço de entrega", () => {
-    // Localize o botão "Add to Cart" do terceiro produto na lista e clique nele
-    cy.get(":nth-child(7) > .btn").click(); //Botão Place holder
-  });
-
-  it("TC06 - Página de confirmação do endereço de entrega", () => {
-    cy.get('[data-qa="name-on-card"]').type("Bruno");
-    cy.get('[data-qa="card-number"]').type("2020303055552222");
-    cy.get('[data-qa="cvc"]').type("222");
-    cy.get('[data-qa="cvc"]').type("2025");
-    cy.get('[data-qa="expiry-year"]').type("2023");
-    cy.get('[data-qa="pay-button"]').click();
+    cy.get('.features_items > :nth-child(3) > .product-image-wrapper > .single-products > .productinfo > .btn').click().wait(2000);
+    cy.get('.modal-footer > .btn').click().wait(2000);
+    cy.visit("https://automationexercise.com/view_cart");
+    //Segue para a página de confirmação do endereço
+    cy.get('.col-sm-6 > .btn').click();
+    //Segue para a página de confirmação de pagamento com cartão
+    cy.get(':nth-child(7) > .btn').click();
+ 
+    //Preenchimento dos dados do Cartão de Crédito
+    const name = 'bruno';
+    const carNumber = '2020303055552222';
+    const cvc = '222';
+    const expiryMonth ='05';
+    const expiryYear = '2025';
+    
+    cy.get('[name="name_on_card"]').type(name);
+    cy.get('[name="card_number"]').type(carNumber);
+    cy.get('[name="cvc"]').type(cvc);
+    cy.get('[name="expiry_month"]').type(expiryMonth);
+    cy.get('[name="expiry_year"]').type(expiryYear);
+    cy.get('[id="submit"]').click();
   });
 });
